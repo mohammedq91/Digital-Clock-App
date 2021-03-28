bb95aee43827a78cf8bbc10e46004bde92a2c51c
Mohammed <mohammedq91@gmail.com>
2021-03-28 19:32:48 -0400

    added p tag in html

--- js.js
+++ js.js
@@ -1,18 +1,20 @@
-
-
-
 function clock(){
   const date = new Date();
+
   const hour = date.getHours();
   const minute = date.getMinutes();
   const second = date.getSeconds();
+
+  let timeDisplay = document.getElementById("clock");
+
   const day = date.getDay();
   const month = date.getMonth();
   const date = date.getDate();
   const year = date.getFullYear();
+
   let timeString = `Today is : ${hour}:${minute}:${second}+'/' 
                       ${day}, ${month} ${date}+'th' ${year}`
-  let time = document.getElementById("clock")
+  let dateDisplay = document.getElementById("date")
   time.append(clock);
 };
 clock();
