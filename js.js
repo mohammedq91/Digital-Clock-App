ed29cdcccd80239c3abd6757f69a97812639ae5d
Mohammed <mohammedq91@gmail.com>
2021-03-28 15:43:19 -0400

    appending clock string in JS file

--- js.js
+++ js.js
@@ -1,11 +1,17 @@
 let time = document.getElementById("clock")
+time.append(p)
 
 function clock(){
   const date = new Date();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const seconds = date.getSeconds();
-  time.textcontent = `${hours}:${minutes}:${seconds}`
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
+  const day = date.getDay();
+  const month = date.getMonth();
+  const date = date.getDate();
+  const year = date.getFullYear();
+  time.textcontent = `${hour}:${minute}:${second}+'/' 
+                      ${day}, ${month} ${date}+'th' ${year} `
 
 };
 clock();
