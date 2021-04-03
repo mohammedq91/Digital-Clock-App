57fb28187f9b93f8d0f139cadca293937ab7e552
Mohammed <mohammedq91@gmail.com>
2021-04-03 19:54:24 -0400

    Editing digital clock files

--- script.js
+++ script.js
@@ -1,27 +1,85 @@
-// grab clock ID from html (done)
-//make clock (done)
 function clock(){
-const clockContainer = document.getElementById('Clock');
-const today = new Date();
+  const today = new Date();
 
+  const day = getDayByIndex(today.getDay());
+  const month = getMonthByIndex(today.getMonth());
+  const date = addDateSuffix(today.getDate());
+  const year = today.getFullYear();
 
+  let dateDisplay = document.getElementById('displayDate');
+  dateDisplay.textContent = `${day}, ${month} ${date} ${year}`;
 
+  const militaryHours = addLeadingZero(today.getHours());
+  const standardHours = getStandardHours(militaryHours);
+  const minutes = addLeadingZero(today.getMinutes());
+  const seconds = addLeadingZero(today.getSeconds());
 
-// shorter way
-const militaryHours = addLeadingZero(today.getHours());
-const minutes = addLeadingZero(today.getMinutes());
-const seconds = addLeadingZero(today.getSeconds());
-const time = `${militaryHours}:${minutes}:${seconds}`;
-//console.log(militaryHours);
-//const hours = date.getHours();
-
-
-//const time =`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-//clockContainer.textContent = time;
-
+  let timeDisplay = document.getElementById('displayTime');
+  timeDisplay = `${standardHours}:${minutes}:${seconds} ${militaryHours > 12 ? 'PM' : 'AM'}`;
 }
+
+function addLeadingZero(time){
+  if (time < 10) {
+    return "0" + time;
+  } else {
+    return time;
+  }
+}
+
+function getStandardHours(militaryHours){
+  if (militaryHours ===0){
+    return 12;
+  } else if (militaryHours > 12){
+    return militaryHours-12;
+  } else {
+    return militaryHours;
+  }
+}
+
+function addDateSuffix(date){
+  if (date === 1 || date === 21 || date === 31){
+    return date + "st";
+  } else if (date === 2 || date === 22){
+    return date + "nd";
+  } else if (date === 3 || date === 23){
+    return date + "rd";
+  } else {
+    return date + "th";
+  }
+}
+ 
+function getMonthByIndex(index){
+  const months = [
+    "January",
+    "Februray",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+  return months[index];
+}
+
+function getDayByIndex(index){
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  return days[index];
+}
+
+setInterval(clock,1000);
 clock();
 
-//clock tick (done)
-setInterval(clock,1000);
 
